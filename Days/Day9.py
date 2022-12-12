69b57ce7be92c809a5de1cae298400de0ff0c0f3
nicqx <miklos.d.parrag@gmail.com>
2022-12-12 19:05:12 +0100

    day9

--- Days/Day9.py
+++ Days/Day9.py
@@ -1,12 +1,10 @@
 class Day9:
     commands = []
-    h = [0, 0]
-    t = [0, 0]
+    rope = []
     visited = set()
 
     def __init__(self, file):
-        self.h = [0, 0]
-        self.t = [0, 0]
+        self.rope = []
         self.visited.clear()
         self.commands.clear()
         with open(file) as f:
@@ -15,60 +13,85 @@ class Day9:
                     self.commands.append(line.removesuffix("\n"))
 
     def task1(self):
+        self.rope.clear()
+        self.visited.clear()
         self.visited.add((0, 0))
+        for i in range(2):
+            self.rope.append([0, 0])
+        self.process_commands()
+
+        return str(len(self.visited))
+
+    def task2(self):
+        self.rope.clear()
+        self.visited.clear()
+        self.visited.add((0, 0))
+        for i in range(10):
+            self.rope.append([0, 0])
+        self.process_commands()
+        return str(len(self.visited))
+
+    def process_commands(self):
         for command in self.commands:
             match (command.split(" ")[0]):
                 case "R":
                     for i in range(int(command.split(" ")[1])):
-                        self.h[0] += 1
-                        self.check_if_t_needs_move()
+                        self.rope[0][0] += 1
+                        for j in range(1, len(self.rope)):
+                            self.rope[j] = self.check_if_tail_needs_move(self.rope[j - 1], self.rope[j])
                 case "L":
                     for i in range(int(command.split(" ")[1])):
-                        self.h[0] -= 1
-                        self.check_if_t_needs_move()
+                        self.rope[0][0] -= 1
+                        for j in range(1, len(self.rope)):
+                            self.rope[j] = self.check_if_tail_needs_move(self.rope[j - 1], self.rope[j])
                 case "U":
                     for i in range(int(command.split(" ")[1])):
-                        self.h[1] += 1
-                        self.check_if_t_needs_move()
+                        self.rope[0][1] += 1
+                        for j in range(1, len(self.rope)):
+                            self.rope[j] = self.check_if_tail_needs_move(self.rope[j - 1], self.rope[j])
                 case "D":
                     for i in range(int(command.split(" ")[1])):
-                        self.h[1] -= 1
-                        self.check_if_t_needs_move()
+                        self.rope[0][1] -= 1
+                        for j in range(1, len(self.rope)):
+                            self.rope[j] = self.check_if_tail_needs_move(self.rope[j - 1], self.rope[j])
 
-        return str(len(self.visited))
-
-    def check_if_t_needs_move(self):
+    def check_if_tail_needs_move(self, head, tail):
         # right
-        if self.h[0] == self.t[0] + 2 and self.h[1] == self.t[1]:
-            self.t[0] += 1
+        if head[0] == tail[0] + 2 and head[1] == tail[1]:
+            tail[0] += 1
         # left
-        elif self.h[0] == self.t[0] - 2 and self.h[1] == self.t[1]:
-            self.t[0] -= 1
+        elif head[0] == tail[0] - 2 and head[1] == tail[1]:
+            tail[0] -= 1
         # up
-        elif self.h[1] == self.t[1] + 2 and self.h[0] == self.t[0]:
-            self.t[1] += 1
+        elif head[1] == tail[1] + 2 and head[0] == tail[0]:
+            tail[1] += 1
         # down
-        elif self.h[1] == self.t[1] - 2 and self.h[0] == self.t[0]:
-            self.t[1] -= 1
+        elif head[1] == tail[1] - 2 and head[0] == tail[0]:
+            tail[1] -= 1
         # up right
-        elif (self.h[0] == self.t[0] + 1 and self.h[1] == self.t[1] + 2) or (
-                self.h[0] == self.t[0] + 2 and self.h[1] == self.t[1] + 1):
-            self.t[0] += 1
-            self.t[1] += 1
+        elif (head[0] == tail[0] + 1 and head[1] == tail[1] + 2) or (
+                head[0] == tail[0] + 2 and head[1] == tail[1] + 1) or (
+                head[0] == tail[0] + 2 and head[1] == tail[1] + 2):
+            tail[0] += 1
+            tail[1] += 1
         # up left
-        elif (self.h[0] == self.t[0] - 1 and self.h[1] == self.t[1] + 2) or (
-                self.h[0] == self.t[0] -2 and self.h[1] == self.t[1] +1):
-            self.t[0] -= 1
-            self.t[1] += 1
+        elif (head[0] == tail[0] - 1 and head[1] == tail[1] + 2) or (
+                head[0] == tail[0] - 2 and head[1] == tail[1] + 1) or (
+                head[0] == tail[0] - 2 and head[1] == tail[1] + 2):
+            tail[0] -= 1
+            tail[1] += 1
         # down right
-        elif (self.h[0] == self.t[0] +1 and self.h[1] == self.t[1] -2) or (
-                self.h[0] == self.t[0] + 2 and self.h[1] == self.t[1] - 1):
-            self.t[0] += 1
-            self.t[1] -= 1
+        elif (head[0] == tail[0] + 1 and head[1] == tail[1] - 2) or (
+                head[0] == tail[0] + 2 and head[1] == tail[1] - 1) or (
+                head[0] == tail[0] + 2 and head[1] == tail[1] - 2):
+            tail[0] += 1
+            tail[1] -= 1
         # down left
-        elif (self.h[0] == self.t[0] - 2 and self.h[1] == self.t[1] - 1) or (
-                self.h[0] == self.t[0] - 1 and self.h[1] == self.t[1] - 2):
-            self.t[0] -= 1
-            self.t[1] -= 1
+        elif (head[0] == tail[0] - 2 and head[1] == tail[1] - 1) or (
+                head[0] == tail[0] - 1 and head[1] == tail[1] - 2) or (
+                head[0] == tail[0] - 2 and head[1] == tail[1] - 2):
+            tail[0] -= 1
+            tail[1] -= 1
 
-        self.visited.add((self.t[0], self.t[1]))
+        self.visited.add((self.rope[-1][0], self.rope[-1][1]))
+        return tail
--- Tests/test_Day9.py
+++ Tests/test_Day9.py
@@ -9,12 +9,20 @@ class TestDay9(TestCase):
         actual_value = Day9('../Resources/Day9/test').task1()
         self.assertEqual(expected_value, actual_value)
 
+    def test_task2(self):
+        expected_value = str(1)
+        actual_value = Day9('../Resources/Day9/test').task2()
+        self.assertEqual(expected_value, actual_value)
+        expected_value = str(36)
+        actual_value = Day9('../Resources/Day9/test2').task2()
+        self.assertEqual(expected_value, actual_value)
+
     def test_original(self):
         expected_value_task1 = str(5619)
-        expected_value_task2 = str(2250)
+        expected_value_task2 = str(2376)
         d9 = Day9('../Resources/Day9/input')
         actual_value_task1 = d9.task1()
         self.assertEqual(expected_value_task1, actual_value_task1)
-        # d9 = Day9('../Resources/Day9/input')
-        # actual_value_task2 = d9.task2()
-        # self.assertEqual(expected_value_task2, actual_value_task2)
+        d9 = Day9('../Resources/Day9/input')
+        actual_value_task2 = d9.task2()
+        self.assertEqual(expected_value_task2, actual_value_task2)
